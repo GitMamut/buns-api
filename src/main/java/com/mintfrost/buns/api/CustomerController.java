105f5b490952f43a1ae51596db5a167cda6403ff
Przemek <vatier@gazeta.pl>
2024-07-27 19:56:16 +0200

    tune cusotmer controler

--- src/main/java/com/mintfrost/buns/api/CustomerController.java
+++ src/main/java/com/mintfrost/buns/api/CustomerController.java
@@ -2,9 +2,10 @@ package com.mintfrost.buns.api;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +13,22 @@ import com.mintfrost.buns.api.persistance.Customer;
 import com.mintfrost.buns.api.persistance.CustomerRepository;
 
 @RestController
-public class TestDbController {
+public class CustomerController {
 
     @Autowired
-    CustomerRepository repository;
+    CustomerRepository customerRepository;
 
-    @GetMapping("/testdb")
-    public List<Customer> CustomerRespose() {
+    @GetMapping("/customer")
+    public ResponseEntity<List<Customer>> getCustomers() {
         List<Customer> customersList = new ArrayList<>();
 
-        StreamSupport.stream(repository.findAll().spliterator(), false)
+        customerRepository.findAll()
                 .forEach(customersList::add);
 
-        return customersList;
+        if (customersList.isEmpty()) {
+            return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(customersList, HttpStatus.OK);
     }
 }
