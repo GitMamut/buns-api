985bf0fa8a8b88505dc7c0eeb84355e4f1f915bc
Przemek <vatier@gazeta.pl>
2024-08-29 19:19:14 +0200

    remove customer repository

--- src/main/java/com/mintfrost/buns/api/persistance/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mintfrost.buns.api.persistance;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
-    
-}
